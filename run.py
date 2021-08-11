c17c6f5eee302fece3d2cbb83de454a2ac5b39c8
deano98 <deano_98@hotmail.com>
2021-08-11 18:07:16 +0000

    Add function to input and store player names

--- run.py
+++ run.py
@@ -1,1 +1,12 @@
-# python code goes here
+def get_player_names():
+    """
+    Get the names from both players
+    """
+    player_one = input("Player 1 Name:\n")
+    player_two = input("Player 2 Name:\n")
+
+    return player_one, player_two
+
+
+players = get_player_names()
+print(f"Player One: {players[0]} vs Player 2: {players[1]}")
