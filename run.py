c0d0d988de1b7b914a8edc71666e89f5cb8bd8b0
deano98 <deano_98@hotmail.com>
2021-08-12 17:24:20 +0000

    Create empty board variables and add a function which asks player one to input their ship coordinates

--- run.py
+++ run.py
@@ -1,3 +1,21 @@
+from pprint import pprint
+
+player_one_board = [
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+]
+
+player_two_board = [
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' '],
+]
+
 def get_player_names():
     """
     Get the names from both players
@@ -8,5 +26,34 @@ def get_player_names():
     return player_one, player_two
 
 
-players = get_player_names()
-print(f"Player One: {players[0]} vs Player 2: {players[1]}")
+def player_one_ships():
+    """
+    Get the ship coordinate input from player 1
+    """
+    coordinates = []
+    
+    for i in range(5):
+        print(f"Please enter the coordinates for ship number {i + 1}")
+        print("Coordinates should be A-E followed by 1-5")
+        print("For example C2")
+
+        player_one_coordinates = input("Enter your coordinates here:\n")
+
+        coordinates.append(player_one_coordinates)
+
+    return coordinates
+
+
+def main():
+    """
+    Run all program functions
+    """
+    players = get_player_names()
+    print(f"Player One: {players[0]} vs Player 2: {players[1]}")
+    pprint(player_one_board)
+    pprint(player_two_board)
+    p1_coordinates = player_one_ships()
+    print(p1_coordinates)
+
+
+main()
