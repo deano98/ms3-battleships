26f055c67cfabef5c6fabce3e85c701abf89b5b3
deano98 <deano_98@hotmail.com>
2021-08-14 15:59:53 +0000

    Modify and add parameters to functions so they can be used for both players

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ player_two_board = [
     [' ', ' ', ' ', ' ', ' '],
 ]
 
+
 def get_player_names():
     """
     Get the names from both players
@@ -26,21 +27,18 @@ def get_player_names():
     return player_one, player_two
 
 
-def player_one_ships():
+def player_ships(name):
     """
     Get the ship coordinate input from player 1
     """
     coordinates = []
     while True:
         i = 1
-        while i < 6:
-            """
-            print(f"Please enter the coordinates for ship number {i}")
-            print("Coordinates should be A-E followed by 1-5")
-            print("For example C2")
-            """
 
-            player_one_coordinates = input("Enter your coordinates here:\n")
+        print("Coordinates should be A-E followed by 1-5")
+        print("For example C2")
+        while i < 6:
+            player_one_coordinates = input(f"{name} Please enter the coordinates for ship number {i}\n")
 
             if validate_coordinates(player_one_coordinates):
                 print("Coordinates are Valid!")
@@ -59,7 +57,7 @@ def validate_coordinates(values):
     try:
         if len(values) != 2:
             raise ValueError("Please type 2 values for your coordinates")
-        elif values[0] not in "ABCDE":
+        elif values[0] not in "ABCDEabcde":
             raise ValueError("Invalid coordinates")
         elif values[1] not in "12345":
             raise ValueError("Invalid coordinates")
@@ -70,15 +68,15 @@ def validate_coordinates(values):
     return True
 
 
-def process_coordinates(coordinates):
+def process_coordinates(coordinates, board):
     """
     Processes the coordinates given and adds them to the board
     """    
     i = 0
     while i < 5:
-        x = int(ord(coordinates[i][0])) - 65
+        x = int(ord(coordinates[i][0].upper())) - 65
         y = int(coordinates[i][1]) - 1
-        print(x, y)
+        board[x][y] = "S"
         i += 1
 
 
@@ -88,11 +86,16 @@ def main():
     """
     players = get_player_names()
     print(f"Player One: {players[0]} vs Player 2: {players[1]}")
-    pprint(player_one_board)
-    pprint(player_two_board)
-    p1_coordinates = player_one_ships()
+    p1_coordinates = player_ships(players[0])
+    p2_coordinates = player_ships(players[1])
     print(p1_coordinates)
-    new_board = process_coordinates(p1_coordinates)
+    print(p2_coordinates)
+    process_coordinates(p1_coordinates, player_one_board)
+    print(f"{players[0]} board:")
+    pprint(player_one_board)
+    process_coordinates(p2_coordinates, player_two_board)
+    print(f"{players[1]} board:")
+    pprint(player_two_board)
 
 
 main()
